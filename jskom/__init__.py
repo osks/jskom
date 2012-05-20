6526d2f5f4613be92c530d0b04fd1f43f7610b0a
Oskar Skoog <oskar@osd.se>
2012-05-20 18:25:39 +0200

    Fixed default settings object

--- jskom/__init__.py
+++ jskom/__init__.py
@@ -3,20 +3,19 @@
 from flask import Flask, render_template
 
 
-default_settings = {
+class default_settings:
     # httpkom server, without trailing slash (example: 'http://localhost:5001')
-    'HTTPKOM_SERVER': 'http://localhost:5001'
-}
+    HTTPKOM_SERVER = 'http://localhost:5001'
 
 
 app = Flask(__name__)
-app.config.from_object('jskom.default_settings')
+app.config.from_object(default_settings)
 app.config.from_envvar('JSKOM_SETTINGS')
-
 
 
 @app.route("/", defaults={ 'path': '' })
 @app.route("/<path:path>")
-def jskom(path):
+def index(path):
     # pth is for html5 push state
+    app.logger.debug(app.config)
     return render_template('index.html', httpkom_server=app.config['HTTPKOM_SERVER'])
