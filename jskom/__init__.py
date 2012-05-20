c47da594b402e4dd29335e00d51125cacd26cd83
Oskar Skoog <oskar@osd.se>
2012-05-20 19:30:33 +0200

    Removed forgotten debug logging.

--- jskom/__init__.py
+++ jskom/__init__.py
@@ -17,5 +17,4 @@ app.config.from_envvar('JSKOM_SETTINGS')
 @app.route("/<path:path>")
 def index(path):
     # pth is for html5 push state
-    app.logger.debug(app.config)
     return render_template('index.html', httpkom_server=app.config['HTTPKOM_SERVER'])
