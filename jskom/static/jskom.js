d58d140972f81d8a48990db477cc24dc35e9bb28
Oskar Skoog <oskar@osd.se>
2012-06-03 21:32:24 +0200

    Indented code as Emacs wants it.

--- jskom/static/jskom.js
+++ jskom/static/jskom.js
@@ -5,101 +5,101 @@
 
 (function($, _, Backbone, Handlebars) {
 
-var jskom;    
-jskom = window.jskom = {
-    version: "0.1",
-    
-    Routers: {},
-    Models: {},
-    Collections: {},
-    Views: {},
-    
-    // httpkom server URL without trailing slash (example: 'http://localhost:5001')
-    Settings: {
-        HttpkomServer: "",
-        PrefetchCount: 2
-    },
-    
-    Log: {
-        debug: function() {
-            if (window.console && console.log) {
-                console.log.apply(console, arguments);
+    var jskom;    
+    jskom = window.jskom = {
+        version: "0.1",
+        
+        Routers: {},
+        Models: {},
+        Collections: {},
+        Views: {},
+        
+        // httpkom server URL without trailing slash (example: 'http://localhost:5001')
+        Settings: {
+            HttpkomServer: "",
+            PrefetchCount: 2
+        },
+        
+        Log: {
+            debug: function() {
+                if (window.console && console.log) {
+                    console.log.apply(console, arguments);
+                }
             }
-        }
-    },
-    
-    init: function() {
-        var jskomUrlRoot = '/';
-        jskom.Models.Session.fetchCurrentSession(function(currentSession) {
-            jskom.router = new jskom.Routers.AppRouter({
-                currentSession: currentSession,
-                urlRoot: jskomUrlRoot
+        },
+        
+        init: function() {
+            var jskomUrlRoot = '/';
+            jskom.Models.Session.fetchCurrentSession(function(currentSession) {
+                jskom.router = new jskom.Routers.AppRouter({
+                    currentSession: currentSession,
+                    urlRoot: jskomUrlRoot
+                });
+                Backbone.history.start({ pushState: true, root: jskomUrlRoot });
             });
-            Backbone.history.start({ pushState: true, root: jskomUrlRoot });
-        });
-    },
-};
-    
-var checkBrowser = function() {
-    var supported = true;
-    var ul = $("<ul></ul>");
-    if (!$.support.ajax) {
-            supported = false;
-        $(ul).append("<li>Ajax</li>");
-    }
-    if (!$.support.cors) {
-        supported = false;
-        $(ul).append("<li>CORS</li>");
-    }
-    
-    if (!supported) {
-        $('body').empty().append("<div></div>");
-        $('body div')
-            .append('<h3>Your browser is too old for jskom</h3>')
-            .append('Missing support for:')
-            .append(ul);
-        return false;
-    } else {
-        return true;
-    }
-};
-
-$(function() {
-    if (checkBrowser()) {
-        jskom.init();
-    }
-});
-
-
-$.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
-    options.url = jskom.Settings.HttpkomServer + options.url;
-    
-    options.xhrFields = {
-        withCredentials: true
+        },
     };
-});
+    
+    var checkBrowser = function() {
+        var supported = true;
+        var ul = $("<ul></ul>");
+        if (!$.support.ajax) {
+            supported = false;
+            $(ul).append("<li>Ajax</li>");
+        }
+        if (!$.support.cors) {
+            supported = false;
+            $(ul).append("<li>CORS</li>");
+        }
+        
+        if (!supported) {
+            $('body').empty().append("<div></div>");
+            $('body div')
+                .append('<h3>Your browser is too old for jskom</h3>')
+                .append('Missing support for:')
+                .append(ul);
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    $(function() {
+        if (checkBrowser()) {
+            jskom.init();
+        }
+    });
 
 
-Handlebars.registerHelper('url_for', function() {
-    // Example: {{url_for "texts/" text_no}} or {{url_for "texts" "/" "more" "/" text_no}}
-    
-    // Remove the last argument, because it's the options object.
-    var params = _.first(arguments, arguments.length-1)
-    var path = _.reduce(params, function(memo, str){ return memo + str; }, "");
-    //jskom.Log.debug("url_for: " + path);
-    return jskom.router.url(path);
-});
+    $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
+        options.url = jskom.Settings.HttpkomServer + options.url;
+        
+        options.xhrFields = {
+            withCredentials: true
+        };
+    });
 
-Handlebars.registerHelper('text_link', function(text_no, options) {
-    // Example: {{text_link text_no}} or {{text_link text_no text="hej"}}
-    
-    var url = jskom.router.url('texts/' + text_no);
-    var text = (options.hash['text'] || text_no);
-    
-    return new Handlebars.SafeString(
-        '<a class="text-link" href="' + Handlebars.Utils.escapeExpression(url) +
-            '" data-text-no="' + Handlebars.Utils.escapeExpression(text_no) + '">' + 
-            Handlebars.Utils.escapeExpression(text) + '</a>');
-});
+
+    Handlebars.registerHelper('url_for', function() {
+        // Example: {{url_for "texts/" text_no}} or {{url_for "texts" "/" "more" "/" text_no}}
+        
+        // Remove the last argument, because it's the options object.
+        var params = _.first(arguments, arguments.length-1)
+        var path = _.reduce(params, function(memo, str){ return memo + str; }, "");
+        //jskom.Log.debug("url_for: " + path);
+        return jskom.router.url(path);
+    });
+
+    Handlebars.registerHelper('text_link', function(text_no, options) {
+        // Example: {{text_link text_no}} or {{text_link text_no text="hej"}}
+        
+        var url = jskom.router.url('texts/' + text_no);
+        var text = (options.hash['text'] || text_no);
+        
+        return new Handlebars.SafeString(
+            '<a class="text-link" href="' + Handlebars.Utils.escapeExpression(url) +
+                '" data-text-no="' + Handlebars.Utils.escapeExpression(text_no) + '">' + 
+                Handlebars.Utils.escapeExpression(text) + '</a>');
+    });
 
 })(jQuery, _, Backbone, Handlebars);
--- jskom/static/jskom.routers.js
+++ jskom/static/jskom.routers.js
@@ -3,107 +3,107 @@
 "use strict";
 
 (function($, _, Backbone, Handlebars, Routers, Models, Collections, Views, Log, Settings) {
-
-Routers.AppRouter = Backbone.Router.extend({
-    routes: {
-        "": "home",
-        "login": "login",
-        "texts/new": "newText",
-        "texts/:text_no": "showText",
-        //"conferences/:conf_no": "foo",
-        "conferences/:conf_no/unread": "showUnreadTextsInConf",
-        "*path": "home"
-    },
     
-    initialize: function(options) {
-        this.session = options.currentSession;
-        this._setUpSession(this.session);
-        this.urlRoot = options.urlRoot;
+    Routers.AppRouter = Backbone.Router.extend({
+        routes: {
+            "": "home",
+            "login": "login",
+            "texts/new": "newText",
+            "texts/:text_no": "showText",
+            //"conferences/:conf_no": "foo",
+            "conferences/:conf_no/unread": "showUnreadTextsInConf",
+            "*path": "home"
+        },
         
-        this.sessionView = null;
-        this.app = new Views.App().render();
-    },
-    
-    url: function(path) {
-        return this.urlRoot + path;
-    },
-    
-    
-    
-    login: function() {
-        Log.debug('route - login');
-        this.navigate('login', { replace: true });
-        if (this.session) {
-            this.session.destroy({ silent: true }); // destroy / logout any existing session
-        }
-        this.session = new Models.Session({ prefetchCount: 2 });
-        this.sessionView = null;
-        this._setUpSession(this.session);
-        this.app.showMenuView(new Views.Menu({ model: this.session }));
-        this.app.showView(new Views.Login({ model: this.session }));
-    },
-    
-    home: function() {
-        Log.debug('route - home');
-        this.navigate('');
+        initialize: function(options) {
+            this.session = options.currentSession;
+            this._setUpSession(this.session);
+            this.urlRoot = options.urlRoot;
+            
+            this.sessionView = null;
+            this.app = new Views.App().render();
+        },
         
-        this._withSessionView(function() {
-            this.showUnreadConfs();
-        });
-    },
-    
-    newText: function() {
-        Log.debug('route - newText');
-        this.navigate('texts/new');
+        url: function(path) {
+            return this.urlRoot + path;
+        },
         
-        this._withSessionView(function() {
-            this.newText();
-        });
-    },
-    
-    showText: function(text_no) {
-        Log.debug('route - showText(' + text_no + ')');
-        this.navigate('texts/' + text_no);
         
-        this._withSessionView(function() {
-            this.showText(text_no);
-        });
-    },
-    
-    showUnreadTextsInConf: function(conf_no) {
-        Log.debug('route - showUnreadInConf(' + conf_no + ')');
-        this.navigate("conferences/" + conf_no + "/unread");
         
-        this._withSessionView(function() {
-            this.showUnreadTextsInConf(conf_no);
-        });
-    },
-    
-    
-    
-    _setUpSession: function(session) {
-        session.on('login', function() {
-            Log.debug("on login");
-            this.navigate('', { replace: true });
-            this.home();
-        }, this);
-    },
-    
-    _withSessionView: function(callback) {
-        if (!this.session || this.session.isNew()) {
-            //Log.debug('_withSessionView - session is new');
-            this.login();
-        } else {
-            //Log.debug('_withSessionView - session is not new');
-            if (!this.sessionView) {
-                this.sessionView = new Views.Session({ model: this.session })
-                this.app.showMenuView(new Views.Menu({ model: this.session }));
-                this.app.showView(this.sessionView);
+        login: function() {
+            Log.debug('route - login');
+            this.navigate('login', { replace: true });
+            if (this.session) {
+                this.session.destroy({ silent: true }); // destroy / logout any existing session
             }
-            callback.call(this.sessionView);
-        }
-    },
-});
+            this.session = new Models.Session({ prefetchCount: 2 });
+            this.sessionView = null;
+            this._setUpSession(this.session);
+            this.app.showMenuView(new Views.Menu({ model: this.session }));
+            this.app.showView(new Views.Login({ model: this.session }));
+        },
+        
+        home: function() {
+            Log.debug('route - home');
+            this.navigate('');
+            
+            this._withSessionView(function() {
+                this.showUnreadConfs();
+            });
+        },
+        
+        newText: function() {
+            Log.debug('route - newText');
+            this.navigate('texts/new');
+            
+            this._withSessionView(function() {
+                this.newText();
+            });
+        },
+        
+        showText: function(text_no) {
+            Log.debug('route - showText(' + text_no + ')');
+            this.navigate('texts/' + text_no);
+            
+            this._withSessionView(function() {
+                this.showText(text_no);
+            });
+        },
+        
+        showUnreadTextsInConf: function(conf_no) {
+            Log.debug('route - showUnreadInConf(' + conf_no + ')');
+            this.navigate("conferences/" + conf_no + "/unread");
+            
+            this._withSessionView(function() {
+                this.showUnreadTextsInConf(conf_no);
+            });
+        },
+        
+        
+        
+        _setUpSession: function(session) {
+            session.on('login', function() {
+                Log.debug("on login");
+                this.navigate('', { replace: true });
+                this.home();
+            }, this);
+        },
+        
+        _withSessionView: function(callback) {
+            if (!this.session || this.session.isNew()) {
+                //Log.debug('_withSessionView - session is new');
+                this.login();
+            } else {
+                //Log.debug('_withSessionView - session is not new');
+                if (!this.sessionView) {
+                    this.sessionView = new Views.Session({ model: this.session })
+                    this.app.showMenuView(new Views.Menu({ model: this.session }));
+                    this.app.showView(this.sessionView);
+                }
+                callback.call(this.sessionView);
+            }
+        },
+    });
 
 })(jQuery, _, Backbone, Handlebars, jskom.Routers, jskom.Models, jskom.Collections,
    jskom.Views, jskom.Log, jskom.Settings);
