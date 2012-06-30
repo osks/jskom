5cc96f7f662e40ed2b201c83493c4a4bb5ea38b7
Oskar Skoog <oskar@osd.se>
2012-06-30 18:44:54 +0200

    Made a directive for showing the text body.

--- jskom/static/jskom.directives.js
+++ jskom/static/jskom.directives.js
@@ -4,11 +4,12 @@
 
 angular.module('jskom.directives', []).
   directive('jskomA', [
+    // Examples:
     // <jskom:a text-no="{{ text.text_no }}">Text: {{text.text_no}}</jskom:a>
     // <jskom:a conf-no="{{ text.conf_no }}">Conference: {{text.conf_no}}</jskom:a>
     
-    '$location',
-    function($location) {
+    '$log', '$location',
+    function($log, $location) {
       return {
         restrict: 'E',
         replace: true,
@@ -32,9 +33,70 @@ angular.module('jskom.directives', []).
           });
           
           scope.showText = function() {
-            //jskom.Log.debug("jskomA - showText() - iAttrs.href: " + iAttrs.href);
+            $log.log("jskomA - showText() - iAttrs.href: " + iAttrs.href);
             $location.path(iAttrs.href);
           };
+        }
+      };
+    }
+  ]).
+  directive('jskomTextBody', [
+    '$log',
+    function($log) {
+      
+      return {
+        restrict: 'E',
+        template: '' +
+          '<ng-switch on="type" ng-show="text">' +
+          '  <div ng-switch-when="text" ng-bind-html="text.body|formatTextBody"></div>' +
+          '  <div ng-switch-when="image"><img ng-src="{{ imageUrl }}" /></div>' +
+          '  <div ng-switch-default>' +
+          '    [unknown content-type: &quot;{{ text.content_type }}&quot;]' +
+          '</div>' +
+          '</ng-switch>' +
+          '<div ng-hide="text">[no text]</div>' +
+          '',
+        scope: {
+          modelName: '@model'
+        },
+        link: function(scope, iElement, iAttrs) {
+          // Hack so we can set the model name in an attribute. We use
+          // the parent scope, so being able to specify model makes
+          // this directive seem less magical.
+          var currentModelWatcher;
+          scope.$watch('modelName', function(newModelName) {
+            if (currentModelWatcher) {
+              currentModelWatcher.call(this);
+            }
+            
+            if (newModelName) {
+              currentModelWatcher = scope.$watch('$parent.' + newModelName, function(newText) {
+                // Update local scope text with text from parent scope.
+                scope.text = newText;
+              });
+            }
+          });
+          
+          scope.$watch('text', function(newText) {
+            scope.text = newText;
+            
+            if (scope.text) {
+              // todo: make this a function or property on the model.
+              var mimeType = Mimeparse.parseMimeType(newText.content_type);
+              scope.type = mimeType[0];
+            } else {
+              scope.type = '';
+            }
+            
+            if (scope.type == 'image') {
+              // todo: make this a function (or property) on the model.
+              scope.imageUrl = jskom.Settings.HttpkomServer +
+                '/texts/' + scope.text.text_no + '/body';
+            } else {
+              scope.imageUrl = '';
+            }
+          });
+          
         }
       };
     }
--- jskom/static/jskom.filters.js
+++ jskom/static/jskom.filters.js
@@ -2,10 +2,83 @@
 
 'use strict';
 
-angular.module('jskom.filters', []).
+angular.module('jskom.filters', ['ngSanitize']).
   filter('foo', function() {
     return function(input) {
       var out = "foo " + input;
       return out;
     }
-  });
+  }).
+  filter('textMediaType', function() {
+    return function(contentType) {
+      var mime_type = Mimeparse.parseMimeType(contentType);
+      return mime_type[0];
+    }
+  }).
+  filter('textMediaSubType', function() {
+    return function(contentType) {
+      var mime_type = Mimeparse.parseMimeType(contentType);
+      return mime_type[1];
+    }
+  }).
+  filter('formatTextBody', [
+    '$sanitize',
+    function($sanitize) {
+      var escape = {
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': "&quot;",
+        "'": "&#x27;",
+        "`": "&#x60;"
+      };
+      
+      var badChars = /&(?!\w+;)|[<>"'`]/g;
+      var possible = /[&<>"'`]/;
+      
+      var escapeChar = function(chr) {
+        return escape[chr] || "&amp;";
+      };
+      
+      // Escape html tags
+      var escapeExpression = function(string) {
+        if (string == null || string === false) {
+          return "";
+        }
+        if (!possible.test(string)) {
+          return string;
+        }
+        return string.replace(badChars, escapeChar);
+      };
+      
+      return function(rawBody) {
+        var safeBody = escapeExpression(rawBody);
+        //var safeBody = $sanitize(rawBody);
+        safeBody = safeBody.replace(/\r?\n|\r/g, "<br/>");
+        return safeBody;
+      }
+    }
+  ]);
+
+/*
+    getSafeBody: function() {
+      var mime_type = Mimeparse.parseMimeType(this.get('content_type'));
+      var type = mime_type[0];
+      
+      if (type == 'text') {
+        var safeBody = Handlebars.Utils.escapeExpression(this.get('body'));
+        safeBody = safeBody.replace(/\r?\n|\r/g, "<br>");
+        return new Handlebars.SafeString(safeBody);
+      } else if (type == 'image') {
+        var name = "";
+        if (mime_type[2]['name']) {
+          name = mime_type[2]['name'];
+        }
+        
+        var imageUrl = Settings.HttpkomServer + this.url() + '/body';
+        var imageBody = '<img src="' + imageUrl + '" title="'+ name +'" />';
+        return new Handlebars.SafeString(imageBody);
+      } else {
+        return "<unknown content-type: " + this.get('content_type') + ">";
+      }
+    },
+*/
