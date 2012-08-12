98b3831a785588388d66f00b84f2d3f26c5f4e81
Oskar Skoog <oskar@osd.se>
2012-08-12 10:20:00 +0200

    Added version.py that contains the version number for jskom, also used by javascripts. Added webassets with minification and bundling of javascripts and css.

--- jskom/__init__.py
+++ jskom/__init__.py
@@ -4,7 +4,9 @@
 import os
 
 from flask import Flask, render_template
+from flask.ext.assets import Environment, Bundle
 
+import version
 
 class default_settings:
     DEBUG = False
@@ -20,9 +22,46 @@ if 'JSKOM_SETTINGS' in os.environ:
 else:
     app.logger.info("No environment variable JSKOM_SETTINGS found, using default settings.")
 
+assets = Environment(app)
+
+
+js_libs = Bundle('lib/jquery.js',
+                 'lib/jquery.cookie.js',
+                 'lib/json2.js',
+                 'lib/mimeparse.js',
+                 'lib/underscore.js',
+                 'lib/mousetrap.js',
+                 filters='rjsmin',
+                 output='gen/packed_libs.js')
+assets.register('js_libs', js_libs)
+
+js_angular = Bundle('lib/angular.js',
+                    'lib/angular-sanitize.js',
+                    #'lib/angular-resource.js',
+                    filters='rjsmin',
+                    output='gen/packed_angular.js')
+assets.register('js_angular', js_angular)
+
+js_jskom = Bundle('jskom.js',
+                  'jskom.services.js',
+                  'jskom.controllers.js',
+                  'jskom.auth.js',
+                  'jskom.filters.js',
+                  'jskom.directives.js',
+                  filters='rjsmin',
+                  output='gen/packed_jskom.js')
+assets.register('js_jskom', js_jskom)
+
+css_jskom = Bundle('style.css',
+                   filters='cssmin',
+                   output='gen/packed_jskom.css')
+assets.register('css_jskom', css_jskom)
+
 
 @app.route("/", defaults={ 'path': '' })
 @app.route("/<path:path>")
 def index(path):
-    # pth is for html5 push state
-    return render_template('index.html', httpkom_server=app.config['HTTPKOM_SERVER'])
+    # path is for html5 push state
+    return render_template('index.html',
+                           version=version.__version__,
+                           httpkom_server=app.config['HTTPKOM_SERVER'])
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
+from jskom.version import __version__
 
 setup(
     name='jskom',
-    version='0.1',
+    version=__version__,
     author='Oskar Skoog',
     author_email='oskar@osd.se',
     long_description=__doc__,
     packages=['jskom'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask']
+    install_requires=['Flask', 'Flask-Assets', 'webassets', 'cssmin']
 )
