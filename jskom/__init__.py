4c6b26d4991c7665c290e0bcf2cc386c08da0c5f
Oskar Skoog <oskar@osd.se>
2012-12-04 21:01:47 +0100

    Try with different mime type to see if firefox thinks the icon is ok.

--- jskom/__init__.py
+++ jskom/__init__.py
@@ -90,4 +90,4 @@ def index(path):
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               'favicon.ico', mimetype='image/x-icon')
