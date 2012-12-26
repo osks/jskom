9ebb2901e3093fc5effe0240b317eeb0f43a6ea1
Oskar Skoog <oskar@osd.se>
2012-12-26 17:52:59 +0100

    Adjusted Foundation to our needs.

--- jskom/static/javascripts/foundation/app.js
+++ jskom/static/javascripts/foundation/app.js
@@ -20,19 +20,13 @@
     $.fn.placeholder                ? $('input, textarea').placeholder() : null;
   });
 
-  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
-
   // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
-  if (Modernizr.touch && !window.location.hash) {
+/*  if (Modernizr.touch && !window.location.hash) {
     $(window).load(function () {
       setTimeout(function () {
         window.scrollTo(0, 1);
       }, 0);
     });
-  }
+  }*/
 
 })(jQuery, this);
