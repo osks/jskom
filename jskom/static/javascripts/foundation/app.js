ea64dc2cfdf36279ffbb023c912ce6e67f34ad84
Oskar Skoog <oskar@osd.se>
2012-12-27 15:39:47 +0100

    Removed commented out Foundation stuff.

--- jskom/static/javascripts/foundation/app.js
+++ jskom/static/javascripts/foundation/app.js
@@ -20,13 +20,4 @@
     $.fn.placeholder                ? $('input, textarea').placeholder() : null;
   });
 
-  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
-/*  if (Modernizr.touch && !window.location.hash) {
-    $(window).load(function () {
-      setTimeout(function () {
-        window.scrollTo(0, 1);
-      }, 0);
-    });
-  }*/
-
 })(jQuery, this);
