a926b88fc784d74d4ce65cf7b69e94f07e824e8f
Oskar Skoog <oskar@osd.se>
2014-01-06 20:46:28 +0100

    Don't bind to ipv6 localhost (workaround for very slow requests).

--- runserver.py
+++ runserver.py
@@ -5,4 +5,6 @@ from jskom import app
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    # use 127.0.0.1 instead of localhost to avoid delays related to ipv6.
+    # http://werkzeug.pocoo.org/docs/serving/#troubleshooting
+    app.run(host='127.0.0.1', port=5000, debug=True)
