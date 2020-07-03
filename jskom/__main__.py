4c703480fe395ddef5faa6d388a472b7053f26af
Oskar Skoog <oskar@osd.se>
2020-07-03 13:51:23 +0200

    Add debug command line option.

--- jskom/__main__.py
+++ jskom/__main__.py
@@ -22,18 +22,29 @@ def run(host, port):
 
 
 def main():
-    logging.basicConfig(format='%(asctime)s %(levelname)-7s %(name)-15s %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(levelname)-7s %(name)-15s %(message)s', level=logging.INFO)
 
     parser = argparse.ArgumentParser(description='Jskom')
+    parser.add_argument(
+        '--debug', help='Enable debug logging',
+        default=False, action='store_true')
 
     # use 127.0.0.1 instead of localhost to avoid delays related to ipv6.
     # http://werkzeug.pocoo.org/docs/serving/#troubleshooting
-    parser.add_argument('--host', help='Hostname or IP to listen on',
-                        default='127.0.0.1')
-    parser.add_argument('--port', help='Port to listen on',
-                        type=int, default=5000)
+    parser.add_argument(
+        '--host', help='Hostname or IP to listen on',
+        default='127.0.0.1')
+    parser.add_argument(
+        '--port', help='Port to listen on',
+        type=int, default=5000)
 
     args = parser.parse_args()
+    loglevel = logging.DEBUG if args.debug else logging.INFO
+    logging.getLogger().setLevel(loglevel)
+
+    if not args.debug:
+        # asyncio logs quite verbose also on INFO level, so set to WARNING.
+        logging.getLogger('asyncio').setLevel(logging.WARNING)
 
     log.info("Using args: %s", args)
 
