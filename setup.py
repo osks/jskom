a8bb6ff8409b14e13b83e94d79811c9712dfbcf9
Oskar Skoog <oskar@osd.se>
2020-07-03 13:52:33 +0200

    Update version to 0.21

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name='jskom',
-    version='0.20+dev',
+    version='0.21',
     description='Jskom is a web based LysKOM client written in Javascript',
     long_description=long_description,
     long_description_content_type="text/x-rst",
